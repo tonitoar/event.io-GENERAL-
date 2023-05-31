a955c5c72c134644f897ea8a134f5e0a3dec673d
Toni Torredemer <toni.torredemer.argiles@gmail.com>
2023-05-31 18:17:26 +0200

    login,register phase, falta vincular amb el Backend

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import {Route, Routes} from "react-router-dom";
+import './App.css'
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import Layout from "./components/Layout";
+import RegisterPage from "./pages/RegisterPage";
+
+
+function App() {
+
+  return (
+    <Routes>
+        <Route path="/" element={<Layout />}> 
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />  
+          <Route path="/register" element={<RegisterPage />} />  
+        </Route>
+    </Routes>
+
+
+
+    
+  )
+}
+
+export default App
